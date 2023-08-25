eae71c4c36a0be3681f3c2454e006d85a11466c7
victor osunji <victorosunji@gmail.com>
2023-08-24 23:47:24 -0400

    submit commit
     - Added light/dark mode(has icons that display with respective mode)
     - Button to change text color.
     - Button to display more info about umbc, new page includes images
     - Text font is different

--- ios-prework/ViewController.swift
+++ ios-prework/ViewController.swift
@@ -7,14 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIColorPickerViewControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // make light mode default for switch
+        modeIconImageView.image = UIImage(named: "LightMode")
+        
     }
     
-    //Function Description to change background color when "Switch Color" button is pressed
+//    @IBOutlet weak var iconLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var modeIconImageView: UIImageView!
+    @IBOutlet weak var modeSwitch: UISwitch!
+
+
+    //Function to change background color when "Switch Color" button is pressed
     @IBAction func changeBackgroundColor(_ sender: UIButton) {
         func changeColor() -> UIColor {
 
@@ -29,5 +36,65 @@ class ViewController: UIViewController {
         view.backgroundColor = randomColor
     }
     
+    
+
+    
+    //Function to toggle between light and dark mode using silder
+    @IBAction func onClickSwitch(_ sender: UISwitch) {
+        if #available(iOS 13.0, *) {
+            let appDelegate = UIApplication.shared.windows.first
+            if sender.isOn {
+                appDelegate?.overrideUserInterfaceStyle = .dark
+                modeIconImageView.image = UIImage(named: "DarkMode")
+            } else {
+                appDelegate?.overrideUserInterfaceStyle = .light
+                modeIconImageView.image = UIImage(named: "LightMode")
+            }
+            
+            // Animate the light and dark icons to position change
+            UIView.animate(withDuration: 0.1) {
+                var newX: CGFloat = 0.0
+
+                if sender.isOn {
+                    newX = self.modeSwitch.frame.origin.x + self.modeSwitch.frame.width - 25
+                } else {
+                    newX = self.modeSwitch.frame.origin.x + 6
+                }
+
+                self.modeIconImageView.frame.origin.x = newX
+            }
+
+        }
+    }
+
+    
+    
+    //Function to change the text color
+    @IBOutlet weak var colorLabel: UILabel!
+    @IBOutlet weak var schoolLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    
+    func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
+            let selectedColor = viewController.selectedColor
+            colorLabel.textColor = selectedColor
+            schoolLabel.textColor = selectedColor
+            jobLabel.textColor = selectedColor
+            dismiss(animated: true, completion: nil)
+        }
+
+    func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
+        let selectedColor = viewController.selectedColor
+        colorLabel.textColor = selectedColor
+        schoolLabel.textColor = selectedColor
+        jobLabel.textColor = selectedColor
+    }
+    
+    @IBAction func changeTextColorButtonTapped(_ sender: UIButton) {
+        let colorPicker = UIColorPickerViewController()
+        colorPicker.delegate = self
+        present(colorPicker, animated: true, completion: nil)
+    }
 }
+
 
